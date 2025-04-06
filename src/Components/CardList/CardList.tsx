f45fddb93979ebdaf88a51128cb424154b1cc408
Aditya Patel <Adityap902021@gmail.com>
2025-04-06 20:31:43 +1000

    add cardList component to hold all cards

---
+++ src/Components/CardList/CardList.tsx
@@ -0,0 +1,31 @@
+import React, { SyntheticEvent } from 'react'
+import Card from '../Card/Card'
+import { CoinSearch } from '../../../coin';
+import { v4 as uuidv4 } from 'uuid';
+
+interface Props {
+    searchRes: CoinSearch[];
+    onPortfolioCreate: (e: SyntheticEvent) => void;
+}
+
+const CardList = ({searchRes, onPortfolioCreate}: Props) => {
+
+    return (
+        <div className="h-[52vh] overflow-y-auto no-scrollbar border-gray-300 w-full mt-2">
+        {searchRes.length > 0 ? (
+            searchRes.map((res) => (
+            <Card
+                id={res.id}
+                key={uuidv4()}
+                searchRes={res}
+                onPortfolioCreate={onPortfolioCreate}
+            />
+            ))
+        ) : (
+            <h1 className="m-6">No Results</h1>
+        )}
+        </div>
+    );
+}
+
+export default CardList

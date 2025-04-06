d95788b2924b83d68c42d1e0f8cf8510363afa36
Aditya Patel <Adityap902021@gmail.com>
2025-04-06 20:34:16 +1000

    add coin interfaces for a contract

---
+++ coin.d.ts
@@ -0,0 +1,13 @@
+export interface CoinSearch {
+  id: string;
+  name: string;
+  market_cap_rank: number;
+  thumb: string;
+  large: string;
+}
+
+export interface PortCoin {
+  symbol: string;
+  name: string;
+  imgLink: string;
+}

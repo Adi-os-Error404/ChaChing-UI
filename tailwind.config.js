4ebc0b98fe2d6503dd209645274fb661a07b1cb4
Aditya Patel <Adityap902021@gmail.com>
2025-04-06 20:29:21 +1000

    update configs to use tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,16 +4,6 @@ export default {
     "./index.html",
     "./src/**/*.{js,ts,jsx,tsx}",
     ],
-    theme: {
-    extend: {
-        colors: {
-        'cust-light-gray': '#FDFBFB',
-        'cust-gray': '#EEEEEE',
-        'cust-pink': '#FF75A8',
-        'cust-green': '#B3D44F',
-
-        },
-    },
-    },
+    theme: {},
     plugins: [],
 }

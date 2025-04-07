04964609c344c09a11d977d251c3473b14de712e
Aditya Patel <Adityap902021@gmail.com>
2025-04-07 21:00:43 +1000

    define browser router

---
+++ src/Routes/Routes.tsx
@@ -0,0 +1,22 @@
+import { createBrowserRouter } from "react-router-dom";
+import App from "../App";
+import HomePage from "../Pages/HomePage/HomePage";
+import SearchPage from "../Pages/SearchPage/SearchPage";
+import CointPage from "../Pages/CoinPage/CointPage";
+
+export const router = createBrowserRouter([
+    {
+        path: "/",
+        element: <App />,
+        children: [
+            {path: "", element: <HomePage />},
+            {path: "search", element: <SearchPage />},
+            {path: "coin/:ticker", 
+                element: <CointPage />,
+                children: [
+
+                ]
+            },
+        ]
+    }
+]);

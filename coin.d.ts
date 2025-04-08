ae7a179747b92291aee75a82e83c887e41dcbb77
Aditya Patel <Adityap902021@gmail.com>
2025-04-08 23:45:02 +1000

    update coin res interface

--- coin.d.ts
+++ coin.d.ts
@@ -15,21 +15,57 @@ export interface PortCoin {
 }
 
 export interface CoinDetails {
+  id: string;
   name: string;
   symbol: string;
-  image: string;
-  description: string;
-  currentPriceUSD: number;
-  marketCapUSD: number;
-  rank: number;
-  totalVolumeUSD: number;
-  priceChange24h: number;
-  high24h: number;
-  low24h: number;
-  circulatingSupply: number;
-  maxSupply: number | null;
-  athUSD: number;
-  atlUSD: number;
-  homepage: string;
-  whitepaper: string | null;
+  image: {
+    thumb: string;
+    small: string;
+    large: string;
+  };
+  description: {
+    en: string;
+  };
+  public_notice: string;
+  market_cap_rank: number;
+  hashing_algorithm: string | null;
+  block_time_in_minutes: number;
+  genesis_date: string;
+  sentiment_votes_up_percentage: number;
+  sentiment_votes_down_percentage: number;
+  watchlist_portfolio_users: number;
+  market_data: {
+    current_price: {
+      usd: number;
+    };
+    ath: {
+      usd: number;
+    };
+    atl: {
+      usd: number;
+    };
+    market_cap: {
+      usd: number;
+    };
+    high_24h: {
+      usd: number;
+    };
+    low_24h: {
+      usd: number;
+    };
+    total_supply: number;
+    max_supply: number;
+    circulating_supply: number;
+  };
+
+  links: {
+    homepage: Array<string> | null;
+    whitepaper: string | null;
+    subreddit_url: string | null;
+  };
+}
+
+export interface MarketChartPoint {
+  timestamp: number;
+  price: number;
 }

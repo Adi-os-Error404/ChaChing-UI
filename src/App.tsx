1fabfe4624d680a5627e4eea2737b9d0db33f753
Aditya Patel <Adityap902021@gmail.com>
2025-04-17 21:02:42 +1000

    Add ToastContainer to app for error - exceptions

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,9 @@
 import React, { useState, SyntheticEvent} from 'react'
 import './App.css'
+import "react-toastify/dist/ReactToastify.css";
 import Navbar from './Components/Navbar/Navbar'
 import { Outlet } from 'react-router'
+import { ToastContainer } from "react-toastify"
 
 function App() {
 
@@ -12,6 +14,7 @@ function App() {
       <Navbar />
       <div className='pt-32'>
         <Outlet />
+        <ToastContainer />
       </div>
     </>
   )

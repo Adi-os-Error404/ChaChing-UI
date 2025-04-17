1369ed7471c51e5fd25a7a7ae215e89dbf95ae0f
Aditya Patel <Adityap902021@gmail.com>
2025-04-17 21:56:09 +1000

    add register API func

--- src/Services/AuthService.tsx
+++ src/Services/AuthService.tsx
@@ -1,17 +1,41 @@
 import axios from "axios";
-import { UserProfileToken } from "../Models/User";
+import { UserProfile, UserProfileToken } from "../Models/User";
+import { handleError } from "../Helpers/ErrorHandler";
 
-const API = "http://localhost:8080/api/";
+
+const API = "http://localhost:8080/api/account";
 
 export const loginAPI = async (username: string, password: string) => {
     try {
-        const res = await axios.post<UserProfileToken>(API + "account/login", {
+        const res = await axios.post<UserProfileToken>(API + "/login", {
             username: username,
             password: password
         });
+        return res;
     }
     catch (err) {
-
+        handleError(err);
     }
 };
 
+export const registerAPI = async (
+    firstName: string,
+    lastName: string,
+    username: string,
+    email: string,
+    password: string
+) => {
+    try {
+        const res = await axios.post<UserProfile>(API + "/register", {
+            firstName: firstName,
+            lastName: lastName,
+            email: email,
+            username: username,
+            password: password
+        });
+        return res;
+    } 
+    catch (err) {
+        handleError(err);
+    }
+};

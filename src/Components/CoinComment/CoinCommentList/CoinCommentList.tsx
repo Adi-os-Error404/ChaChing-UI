7bfd8f0d2f0b50d654b4b0cda65a62264d69fb62
Aditya Patel <Adityap902021@gmail.com>
2025-04-20 15:13:17 +1000

    update ui and pass handlers

--- src/Components/CoinComment/CoinCommentList/CoinCommentList.tsx
+++ src/Components/CoinComment/CoinCommentList/CoinCommentList.tsx
@@ -1,16 +1,33 @@
 import React from 'react'
-import { CommentDetails } from '../../../Models/Comment'
+import { CommentDetails, CommentFormInputs } from '../../../Models/Comment'
 import CoinCommentItem from '../CoinCommentItem/CoinCommentItem';
+import { UserProfile } from '../../../Models/User';
 
 interface Props {
     comments: CommentDetails[];
+    user: UserProfile;
+    editingId: number | null;
+    onStartEdit: (id: number) => void;
+    onCancelEdit: () => void;
+    onSubmitEdit: (id: number, data: CommentFormInputs) => void;
+    onDelete: (id: number) => void;
 };
 
-const CoinCommentList = ({ comments }: Props) => {
+const CoinCommentList = ({ comments, user, editingId, onStartEdit, onCancelEdit, onSubmitEdit, onDelete }: Props) => {
     return (
         <>
             {comments
-                ? comments.map((c) => <CoinCommentItem key={c.id} comment={c}/> )
+                ? comments.map((c) => 
+                    <CoinCommentItem 
+                        key={c.id} 
+                        comment={c} 
+                        user={user}
+                        isEditing={editingId === c.id}
+                        onStartEdit={onStartEdit}
+                        onCancelEdit={onCancelEdit}
+                        onSubmitEdit={onSubmitEdit}
+                        onDelete={onDelete}
+                    /> )
                 : ""
             }
         </>

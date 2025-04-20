632b3ac86878c202aedf41254b5f035073880faf
Aditya Patel <Adityap902021@gmail.com>
2025-04-20 15:13:34 +1000

    use APIs in handlers and pass it to child components

--- src/Components/CoinComment/CoinComment.tsx
+++ src/Components/CoinComment/CoinComment.tsx
@@ -1,19 +1,22 @@
 import React, { useState } from 'react'
 import CoinCommentForm from './CoinCommentForm/CoinCommentForm'
-import { addCommentAPI, getCommentsAPI } from '../../Services/CommentService';
+import { addCommentAPI, deleteCommentAPI, editCommentAPI, getCommentsAPI } from '../../Services/CommentService';
 import { toast } from 'react-toastify';
 import { CommentDetails, CommentFormInputs } from '../../Models/Comment';
 import CoinCommentList from './CoinCommentList/CoinCommentList';
+import { UserProfile } from '../../Models/User';
 
 
 interface Props {
     coinId: string;
     comments: CommentDetails[];
+    user: UserProfile;
 };
 
-const CoinComment = ({ coinId, comments }: Props) => {
+const CoinComment = ({ coinId, comments, user }: Props) => {
 
     const [newComments, setNewComments] = useState<CommentDetails[]>(comments);
+    const [editingId, setEditingId] = useState<number | null>(null);
 
     const getComments = () => {
         getCommentsAPI(coinId)
@@ -27,7 +30,7 @@ const CoinComment = ({ coinId, comments }: Props) => {
         })
     };
 
-    const handleComment = (e: CommentFormInputs) => 
+    const postComment = (e: CommentFormInputs) => 
         addCommentAPI(coinId, e.title, e.content)
         .then((res) => {
             if (res) {
@@ -38,12 +41,52 @@ const CoinComment = ({ coinId, comments }: Props) => {
         .catch((e) => {
             toast.warning(e);
     });
+
+    const handleStartEdit = (id: number) => setEditingId(id);
+    const handleCancelEdit = () => setEditingId(null);
+
+    const handleEditSubmit = async (id: number, data: CommentFormInputs) => {
+        try {
+            const res = await editCommentAPI(id, data.title, data.content);
+            if (res) {
+                toast.success("Comment updated!");
+                getComments();
+                setEditingId(null);
+            }
+        } catch (err) {
+            toast.error("Failed to update comment");
+        }
+    };
+
+
+    const deleteComment = async (id: number) => {
+        try {
+            const res = await deleteCommentAPI(id);
+            if (res) {
+                toast.success("Comment deleted!");
+                setNewComments(newComments.filter(comment => comment.id !== id));
+            }
+        } catch (err) {
+            toast.error("Failed to delete comment");
+        }
+    };
     
 
     return (
         <>
-            <CoinCommentForm coinId={coinId} handleComment={handleComment} />
-            <CoinCommentList comments={newComments} />
+            <CoinCommentForm coinId={coinId} postComment={postComment} />
+            <div className="mt-12 bg-white rounded-2xl shadow-lg p-8 h-[70vh] overflow-x-scroll">
+            <CoinCommentList 
+                comments={newComments} 
+                user={user} 
+                editingId={editingId}
+                onStartEdit={handleStartEdit}
+                onCancelEdit={handleCancelEdit}
+                onSubmitEdit={handleEditSubmit}
+                onDelete={deleteComment}
+            />
+            </div>
+
         </>
     )
 }

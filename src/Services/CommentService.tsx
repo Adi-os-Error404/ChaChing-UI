e5575cfe7f17f29c85c859c8028a734d0fc0948b
Aditya Patel <Adityap902021@gmail.com>
2025-04-20 15:12:22 +1000

    add PUT and DELETE apis to comment service

--- src/Services/CommentService.tsx
+++ src/Services/CommentService.tsx
@@ -4,12 +4,12 @@ import { handleError } from "../Helpers/ErrorHandler";
 import { CommentDetails, CommentPost } from "../Models/Comment";
 
 
-const API = "http://localhost:8080/api/comments/";
+const API = "http://localhost:8080/api/comments";
 
 
 export const getCommentsAPI = async (coindId: string) => {
     try {
-        const res = await axios.get<CommentDetails[]>(API + coindId);
+        const res = await axios.get<CommentDetails[]>(API + `/${coindId}`);
         return res;
     } catch (err) {
         handleError(err);
@@ -22,10 +22,36 @@ export const addCommentAPI = async (
     content: string
 ) => {
     try {
-        const res = await axios.post<CommentPost>(API + coindId, {
+        const res = await axios.post<CommentPost>(API + `/${coindId}`, {
             title: title,
             content: content
         });
+        return res;
+    } catch (err) {
+        handleError(err);
+    }
+}
+
+export const editCommentAPI = async (
+    id: number, 
+    title: string, 
+    content: string
+) => {
+    try {
+        const res = await axios.put<CommentPost>(API, {
+            id: id, 
+            title: title,
+            content: content
+        });
+        return res;
+    } catch (err) {
+        handleError(err);
+    }
+}
+
+export const deleteCommentAPI = async (id: number) => {
+    try {
+        const res = await axios.delete(`http://localhost:8080/api/comments/${id}`);
         return res;
     } catch (err) {
         handleError(err);

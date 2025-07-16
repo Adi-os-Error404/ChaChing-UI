a63bc4c2b696b619b32dc0259607eaede5747f27
Aditya Patel <Adityap902021@gmail.com>
2025-07-16 22:45:14 +1000

    add inspect button

--- src/Components/Card/Card.tsx
+++ src/Components/Card/Card.tsx
@@ -1,8 +1,7 @@
-import React, { SyntheticEvent } from 'react'
+import React from 'react'
 import { CoinSearch } from '../../../coin';
 import AddPortfolio from '../Portfolio/AddPortfolio/AddPortfolio';
-import { searchCoins } from '../../../api';
-import { Link } from 'react-router-dom';
+import { Link, useNavigate } from 'react-router-dom';
 
 interface Props {
     id: string;
@@ -10,30 +9,52 @@ interface Props {
     onPortfolioCreate: (coinId: string) => void;
 }
 
-const Card = ({id, searchRes, onPortfolioCreate}: Props) => {
-return (
-    <div id='card' className='flex bg-stone-100 h-30 md:w-3/4 md:h-34 rounded-xl justify-between items-center ml-4 my-2 border-solid border-black border-4'>
-    <Link to={`/coin/${id}`}>
-        <div className='flex gap-4 m-4 md:gap-8 md:m-8'>
+const Card = ({ id, searchRes, onPortfolioCreate }: Props) => {
+    const navigate = useNavigate();
 
-                <img className='size-16 mr-2 md:mr-0' src={searchRes.large} alt={searchRes.name}/>
+    const handleInspectClick = (e: React.MouseEvent) => {
+        e.stopPropagation();
+        navigate(`/coin/${id}`);
+    };
 
-                <div className='text-left'>
-                    <h1 className='md:text-lg font-medium mt-1 md:mb-0.5'>{searchRes.name} <br className='block md:hidden'/> ({searchRes.symbol})</h1>
-                    <h2 className='hidden md:block text-sm italic font-medium text-gray-500'>Market Cap Rank: #{searchRes.market_cap_rank}</h2>
+    return (
+        <div id='card' className='flex bg-stone-100 h-30 md:w-3/4 md:h-34 rounded-xl justify-between items-center ml-4 my-2 border-solid border-black border-4'>
+
+            <Link to={`/coin/${id}`} className='flex-grow'>
+                <div className='flex gap-3 m-4 md:gap-8 md:m-8'>
+
+                    <img className='size-14 md:size-16 mr-2 md:mr-0' src={searchRes.large} alt={searchRes.name} />
+
+                    <div className='text-left'>
+                        <h1 className='md:text-lg font-medium mt-1 md:mb-0.5'>
+                            {searchRes.name} <br className='block md:hidden' /> ({searchRes.symbol})
+                        </h1>
+                        <h2 className='hidden md:block text-sm italic font-medium text-gray-500'>
+                            Market Cap Rank: #{searchRes.market_cap_rank}
+                        </h2>
+                    </div>
                 </div>
+            </Link>
+
+            <div className='flex items-center m-4 gap-3 md:gap-6 md:m-8'>
+                <button
+                    onClick={handleInspectClick}
+                    className='bg-blue-400 font-bold text-sm size-10 p-1.5 text-white rounded-xl'
+                >
+                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512"><path fill="#ffffff" d="M256 512A256 256 0 1 0 256 0a256 256 0 1 0 0 512zM216 336l24 0 0-64-24 0c-13.3 0-24-10.7-24-24s10.7-24 24-24l48 0c13.3 0 24 10.7 24 24l0 88 8 0c13.3 0 24 10.7 24 24s-10.7 24-24 24l-80 0c-13.3 0-24-10.7-24-24s10.7-24 24-24zm40-208a32 32 0 1 1 0 64 32 32 0 1 1 0-64z"/></svg>
+                </button>
+
+
+                <AddPortfolio
+                    onPortfolioCreate={onPortfolioCreate}
+                    id={id}
+                    name={searchRes.name}
+                    symbol={searchRes.symbol}
+                    imgLink={searchRes.large}
+                />
+            </div>
         </div>
-    </Link>
-    <AddPortfolio
-        onPortfolioCreate={onPortfolioCreate}
-        id={id}
-        name={searchRes.name}
-        symbol={searchRes.symbol}
-        imgLink={searchRes.large}
-    />
+    );
+};
 
-    </div>
-)
-}
-
-export default Card
+export default Card;
--- src/Components/Portfolio/CardPortfolio/CardPortfolio.tsx
+++ src/Components/Portfolio/CardPortfolio/CardPortfolio.tsx
@@ -1,6 +1,6 @@
 import React, { SyntheticEvent } from 'react'
 import DeletePortfolio from '../DeletePortfolio/DeletePortfolio';
-import { Link } from 'react-router-dom';
+import { Link, useNavigate } from 'react-router-dom';
 
 interface Props {
     id: string;
@@ -11,15 +11,30 @@ interface Props {
 }
 
 const CardPortfolio = ({id,symbol, name, imgLink, onPortfolioDelete}: Props) => {
+    const navigate = useNavigate();
+
+    const handleInspectClick = (e: React.MouseEvent) => {
+        e.stopPropagation();
+        navigate(`/coin/${id}`);
+    };
+
   return (
-      <div id='card' className='flex bg-stone-100 h-30 rounded-xl justify-between items-center mx-14 my-4 border-solid border-black border-4 p-2'>
+      <div id='card' className='flex bg-stone-100 h-30 rounded-xl justify-between items-center ml-14 mr-8 my-4 border-solid border-black border-4'>
         <Link to={`/coin/${id}`}>
-          <div className='flex items-center mr-2 md:mr-0'>
-            <img className='size-12 m-4' src={imgLink} alt={name}/>
+          <div className='flex items-center mr-2'>
+            <img className='size-14 md:size-16 m-4 md:m-6' src={imgLink} alt={name}/>
               <h1 className='md:text-lg mt-1 mb-0.5 font-semibold'>{name} <span className='text-gray-700'>({symbol.toUpperCase()})</span></h1>
           </div>
         </Link>
+        <div className='flex items-center m-4 gap-3 md:gap-5.5 md:m-8'>
+                <button
+                    onClick={handleInspectClick}
+                    className='bg-blue-400 font-bold text-sm size-10 p-1.5 text-white rounded-xl'
+                >
+                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512"><path fill="#ffffff" d="M256 512A256 256 0 1 0 256 0a256 256 0 1 0 0 512zM216 336l24 0 0-64-24 0c-13.3 0-24-10.7-24-24s10.7-24 24-24l48 0c13.3 0 24 10.7 24 24l0 88 8 0c13.3 0 24 10.7 24 24s-10.7 24-24 24l-80 0c-13.3 0-24-10.7-24-24s10.7-24 24-24zm40-208a32 32 0 1 1 0 64 32 32 0 1 1 0-64z"/></svg>
+                </button>
         <DeletePortfolio portfolioVal={id} onPortfolioDelete={onPortfolioDelete} />
+      </div>
       </div>
   )
 }
